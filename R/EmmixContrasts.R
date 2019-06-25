5c0a6cd8afdfd28b2174c3ee88b3ffb39c29c806
Andrew Jones <andrewthomasjones@gmail.com>
2019-06-25 21:52:22 +1000

    more imports

---
+++ R/EmmixContrasts.R
@@ -0,0 +1,43 @@
+#imports
+#'@importFrom limma voom
+NULL
+#'@title 
+#'@description 
+#'@param a
+#'@param b
+#'@details 
+#'
+#'@return [return object]
+#'@seealso \code{\link{emmixwire}} \code{\link{scores.wire}}
+#'@examples
+#'
+#'
+#'@keywords cluster datasets
+
+
+
+#'@export
+emmix.contrasts.fit <- function(a,b){
+    voom(1:10)
+}
+
+NULL
+#'@title 
+#'@description 
+#'@param a
+#'@param b
+#'@details 
+#'
+#'@return [return object]}
+#'@examples
+#'
+#'
+#'@keywords cluster datasets
+#'@export
+bic.fit <- function(a,b){
+
+}
+
+
+
+
